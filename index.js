09603e3907ded540f434a63292795b713b28a33e
Lorenz Krüger <lorenzkrueger@gmail.com>
2024-07-10 16:44:34 +0200

    toggle functions

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // phew… not a lot going on here. Please add some code!
+document.querySelectorAll(".card__button-answer").forEach((button) => {
+  button.addEventListener("click", () => {
+    const answer = button.nextElementSibling;
+    answer.classList.toggle("card__answer--active");
+    button.textContent = answer.classList.contains("card__answer--active")
+      ? "Hide answer"
+      : "Show answer";
+  });
+});
