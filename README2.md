41fb9d67468f05b314a2d3fd2950443be2b33dbf
Lorenz Krüger <lorenzkrueger@gmail.com>
2024-07-11 11:53:09 +0200

    added toogle feature to bookmarks w/o synchronized bookmark function

---
+++ README2.md
@@ -0,0 +1,2 @@
+finished feature 1: toggle of questions and answer
+task for Thursday, 11th of July: toggle for bookmarks
--- index.js
+++ index.js
@@ -1,10 +1,66 @@
 // phew… not a lot going on here. Please add some code!
-document.querySelectorAll(".card__button-answer").forEach((button) => {
-  button.addEventListener("click", () => {
-    const answer = button.nextElementSibling;
-    answer.classList.toggle("card__answer--active");
-    button.textContent = answer.classList.contains("card__answer--active")
-      ? "Hide answer"
-      : "Show answer";
+// document.querySelectorAll(".card__button-answer").forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const answer = button.nextElementSibling;
+//     answer.classList.toggle("card__answer--active");
+//     button.textContent = answer.classList.contains("card__answer--active")
+//       ? "Hide answer"
+//       : "Show answer";
+//   });
+// });
+
+let bookmarks = [];
+// Feature to update bookmarks
+function updateBookmarks() {
+  const bookmarkedSection = document.querySelector("bookmarkedCards");
+  if (bookmarkedSection) {
+    bookmarkedSection.innerHTML = bookmarks
+      .map((bookmark) => bookmark.cardHTML)
+      .join("");
+    addAnswerToggleEventListeners();
+    addBookmarkToggleEventlisteners();
+  }
+}
+
+// function to add answer toggle event-listener
+function addAnswerToggleEventListeners() {
+  document.querySelectorAll(".card__button-answer").forEach((button) => {
+    button.addEventListener("click", () => {
+      const answer = button.nextElementSibling;
+      if (answer.style.display === "block") {
+        answer.style.display = "none";
+        button.textContent = "Show answer";
+      } else {
+        answer.style.display = "block";
+        button.textContent = "Hide answer";
+      }
+    });
   });
-});
+}
+
+// function to add Bookmark-toggle.listener
+function addBookmarkToggleEventlisteners() {
+  document.querySelectorAll(".bookmark").forEach((button, index) => {
+    button.addEventListener("click", () => {
+      const card = button.closest(".card");
+      const question = card.querySelector(".card__question").textContent;
+
+      // toggle bookmark button color
+      button.classList.toggle("bookmark--active");
+
+      // add or remove bookmark buton color
+      if (button.classList.contains("bookmark--active")) {
+        bookmarks.push({ cardHTML: card.outerHTML, question });
+      } else {
+        bookmarks = bookmarks.filter(
+          (bookmark) => bookmark.question !== question
+        );
+      }
+      // update bookmarks
+      updateBookmarks();
+    });
+  });
+}
+// add initial eventlistener
+addAnswerToggleEventListeners();
+addBookmarkToggleEventlisteners();
